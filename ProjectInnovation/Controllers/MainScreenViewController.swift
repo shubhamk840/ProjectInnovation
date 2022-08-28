ce8bcd93bddc9d8a4d2daca77a7fc984c55b40db
Shubham Kushwaha <shubhamk840@gmail.com>
2022-08-28 20:23:50 +0530

    api calls refactored

--- ProjectInnovation/Controllers/MainScreenViewController.swift
+++ ProjectInnovation/Controllers/MainScreenViewController.swift
@@ -16,19 +16,10 @@ class MainScreenViewController: UIViewController {
         refreshButton.layer.cornerRadius = 5
         imageView.layer.cornerRadius = 5
         imageView.isUserInteractionEnabled = false
-        let Services = Services(baseUrl: "https://dog.ceo/api/breeds/image/random")
-        Services.getAllCountriesName(endPoint: "",onSuccess: {
-            (dogsData) in
-            let url = URL(string: dogsData.message ?? "")!
-            self.downloadImage(from: url)
-        }, onFailure : {
-            (error) in
-            print(error)
-        })
-        // Do any additional setup after loading the view.
+        makeApiCall()
     }
     
-    @IBAction func refreshBtnClicked(_ sender: Any) {
+    func makeApiCall() {
         SVProgressHUD.show(currentViewController: self)
         let Services = Services(baseUrl: "https://dog.ceo/api/breeds/image/random")
         Services.getAllCountriesName(endPoint: "",onSuccess: {
@@ -42,30 +33,25 @@ class MainScreenViewController: UIViewController {
             print(error)
         })
     }
+    
+    @IBAction func refreshBtnClicked(_ sender: Any) {
+       makeApiCall()
+    }
     func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
     func downloadImage(from url: URL) {
         print("Download Started")
+        self.refreshButton.isUserInteractionEnabled = false
         getData(from: url) { data, response, error in
             guard let data = data, error == nil else { return }
             print(response?.suggestedFilename ?? url.lastPathComponent)
             print("Download Finished")
-            // always update the UI from the main thread
             DispatchQueue.main.async() { [weak self] in
                 self?.imageView.image = UIImage(data: data)
+                self?.refreshButton.isUserInteractionEnabled = true
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
